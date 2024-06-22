ebb08f503a0f661f7a1a9a2df20a4e494c1a02a9
Esther Avila <esther.abenito@gmail.com>
2024-06-23 01:49:08 +0200

    First version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import Layout from './Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './Home';
+import Header from './Header';
+import Trailer from './Trailer';
+import Reviews from './Reviews';
 
 function App() {
+  const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState();
+
+  const getMovies = () => {
+
+    fetch('http://localhost:8080/api/v1/movies')
+      .then(response => response.json())
+      .then(data => setMovies(data))
+  }
+
+  const getMovieData =  (movieId) => {
+    
+    fetch(`http://localhost:8080/api/v1/movies/${movieId}`)
+      .then(response => response.json())
+      .then(data => setMovie(data))
+      .then(data => setReviews(data.reviews))
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />}></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews} />}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
